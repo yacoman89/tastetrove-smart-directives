8445c9eba43ee25af6b62173abc54a0b7cdefa82
Jacob Yacovelli <jacob@yacohome.com>
2024-02-10 15:36:38 -0500

    feat: added skelton loading to dashboard

--- src/app/components/feature/recipe-list/loading-list-item/loading-list-item.component.ts
+++ src/app/components/feature/recipe-list/loading-list-item/loading-list-item.component.ts
@@ -1,10 +1,14 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
+import { LoadingRatingComponent } from '../../../common/loading-rating/loading-rating.component';
 
 @Component({
   selector: 'tt-loading-list-item',
   templateUrl: './loading-list-item.component.html',
   styleUrl: './loading-list-item.component.scss',
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [LoadingRatingComponent]
 })
-export class LoadingListItemComponent {}
+export class LoadingListItemComponent {
+  @HostBinding('class.w-full') get full(): boolean { return true; }
+}
